64d35578a17c4930c87d3af91ac6a1123df43e05
yagyagoel1 <yagyagoel87@gmail.com>
2025-02-18 00:30:07 +0530

    init added the describe instance for ec2

---
+++ cmd/ec2.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/spf13/cobra"
+)
+
+func ListEC2Instances() {
+	cfg, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		log.Fatalf("unable to load sdk config , %v", err)
+	}
+	client := ec2.NewFromConfig(cfg)
+	resp, err := client.DescribeInstances(context.TODO(), &ec2.DescribeInstancesInput{})
+	if err != nil {
+		log.Fatalf("falied to descirbe the instances %v", err)
+	}
+	for _, reservation := range resp.Reservations {
+		for _, instance := range reservation.Instances {
+			fmt.Printf("Instance ID: %s, Type: %s, State: %s\n",
+				*instance.InstanceId, instance.InstanceType, instance.State.Name)
+		}
+	}
+}
+
+var EC2Cmd = &cobra.Command{
+	Use:   "ec2",
+	Short: "List EC2 instances",
+	Run: func(cmd *cobra.Command, args []string) {
+		ListEC2Instances()
+	},
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/yagyagoel1/AWSInfo/cmd"
+
+func main() {
+	cmd.Execute()
+}
